b0535e63a5cd12881cfa725515936bcf543a9b63
yusa0730 <isizawa5226@gmail.com>
2024-06-12 02:53:28 +0900

    add: phpからmysqlに接続できるように設定追加

---
+++ mysql/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+CREATE USER IF NOT EXISTS 'myuser'@'%' IDENTIFIED BY 'mypassword';
+GRANT ALL PRIVILEGES ON mydatabase.* TO 'myuser'@'%';
+FLUSH PRIVILEGES;
+
+USE mydatabase;
+
+CREATE TABLE IF NOT EXISTS test_table (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
--- php-fpm/src/index.php
+++ php-fpm/src/index.php
@@ -1,4 +1,17 @@
 <?php
-echo "Hello World!";
+$servername = "mysql";
+$username = "myuser";
+$password = "mypassword";
+$dbname = "mydatabase";
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($connection->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
 ?>
 
