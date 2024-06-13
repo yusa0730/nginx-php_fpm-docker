702c2e316593978f239053d091da7845b4cd1e4d
yusa0730 <isizawa5226@gmail.com>
2024-06-13 21:43:27 +0900

    add: 環境変数で接続できるように修正。redisの追加

--- php-fpm/src/index.php
+++ php-fpm/src/index.php
@@ -1,17 +1,30 @@
 <?php
-$servername = "mysql";
-$username = "myuser";
-$password = "mypassword";
-$dbname = "mydatabase";
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
 
 // Create connection
 $connection = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($connection->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("MySQL Connection failed: " . $connection->connect_error);
 }
 
-echo "Connected successfully";
+echo "MySQL Connected successfully<br>";
+
+// Redis connection details
+$redis_host = getenv("REDIS_HOST");
+$redis_port = getenv("REDIS_PORT");
+
+// Create Redis connection
+$redis = new Redis();
+try {
+  $redis->connect($redis_host, $redis_port);
+  echo "Redis Connected successfully<br>";
+} catch (Exception $e) {
+  die("Redis Connection failed: " . $e->getMessage());
+}
 ?>
 
