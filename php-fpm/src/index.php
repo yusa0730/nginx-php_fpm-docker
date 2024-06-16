e465fc877eaf5f83d1fa9fdf65be602e7032e615
yusa0730 <isizawa5226@gmail.com>
2024-06-17 02:25:59 +0900

    fix: コメントアウト消去

--- php-fpm/src/index.php
+++ php-fpm/src/index.php
@@ -1,9 +1,4 @@
 <?php
-// $servername = getenv("DB_HOST");
-// $username = getenv("DB_USER");
-// $password = getenv("DB_PASSWORD");
-// $dbname = getenv("DB_NAME");
-
 $servername = $_SERVER['DB_HOST'];
 $username = $_SERVER['DB_USER'];
 $password = $_SERVER['DB_PASSWORD'];
@@ -26,15 +21,8 @@ if ($connection->connect_error) {
 
 echo "MySQL Connected successfully<br>";
 
-// Redis connection details
-// $redis_host = getenv("REDIS_HOST");
-// $redis_port = getenv("REDIS_PORT");
-
 $redis_host = $_SERVER['REDIS_HOST'];
 $redis_port = $_SERVER['REDIS_PORT'];
-
-// Create Redis connection using Predis client
-// require 'vendor/autoload.php';
 
 // Create Redis connection
 $redis = new Redis();
