6f3ea76ebe043409008c468d3e0001c6b4bc52a2
yusa0730 <isizawa5226@gmail.com>
2024-06-17 02:21:11 +0900

    fix: redisの接続処理追加

--- php-fpm/src/index.php
+++ php-fpm/src/index.php
@@ -1,11 +1,23 @@
 <?php
-$servername = getenv("DB_HOST");
-$username = getenv("DB_USER");
-$password = getenv("DB_PASSWORD");
-$dbname = getenv("DB_NAME");
+// $servername = getenv("DB_HOST");
+// $username = getenv("DB_USER");
+// $password = getenv("DB_PASSWORD");
+// $dbname = getenv("DB_NAME");
+
+$servername = $_SERVER['DB_HOST'];
+$username = $_SERVER['DB_USER'];
+$password = $_SERVER['DB_PASSWORD'];
+$dbname = $_SERVER['DB_NAME'];
+$port = $_SERVER['DB_PORT'];
 
 // Create connection
-$connection = new mysqli($servername, $username, $password, $dbname);
+$connection = new mysqli(
+  $servername,
+  $username,
+  $password,
+  $dbname,
+  $port
+);
 
 // Check connection
 if ($connection->connect_error) {
@@ -15,8 +27,14 @@ if ($connection->connect_error) {
 echo "MySQL Connected successfully<br>";
 
 // Redis connection details
-$redis_host = getenv("REDIS_HOST");
-$redis_port = getenv("REDIS_PORT");
+// $redis_host = getenv("REDIS_HOST");
+// $redis_port = getenv("REDIS_PORT");
+
+$redis_host = $_SERVER['REDIS_HOST'];
+$redis_port = $_SERVER['REDIS_PORT'];
+
+// Create Redis connection using Predis client
+// require 'vendor/autoload.php';
 
 // Create Redis connection
 $redis = new Redis();
---
+++ test.md
@@ -0,0 +1,1 @@
+docker build --platform linux/amd64 -t ishizawa-test-dev-ecr-nginx .
